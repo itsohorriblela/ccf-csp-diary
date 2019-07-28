3b81f0445a6c8f425f12ec44c814431794f62dec
horriblela <50759003+itsohorriblela@users.noreply.github.com>
2019-07-28 20:30:59 +0800

    Update 02买菜.cpp

--- 201809/02买菜.cpp
+++ 201809/02买菜.cpp
@@ -1,7 +1,9 @@
+//20180902题目即为求最小的右端值减去最大的左端值，用了vector去做，但感觉定义一个time[4000]的数组应该也可以。vector从标准输入流中初始化？王少奇只会
+//先用临时变量把标准输入流中的值取出来再用push_back，感觉比较麻烦
 #include<vector>
 #include<iostream>
 using namespace std;
-#define max(x,y) ( x>y?x:y )
+#define max(x,y) ( x>y?x:y )//20180902用三目运算符定义宏来快速定义min和max。#define max(x,y) ( x>y?x:y ) 当x>y时取x反之取y
 #define min(x,y) ( x<y?x:y )
 
 int main(){
